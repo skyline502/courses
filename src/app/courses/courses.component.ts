a16f069c7714f802c2f340585e6cc9139fcf5aab
Johnny <johnny.san@gmail.com>
2022-07-15 14:44:22 -0700

    imported stylesheet

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from './courses.service';
 
+
 @Component({
   selector: 'app-courses',
   template: `
   <h2>{{title}}</h2>
-  <ul>
+  <ul class='list'>
     <li *ngFor='let course of courses'>
       {{ course }}
     </li>
   </ul>
-  `
+  `,
+  styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
   title = "List of Courses";
