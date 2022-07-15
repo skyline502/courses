4b666a4a8624017f1646e4955ac3c6c8df9b4850
Johnny <johnny.san@gmail.com>
2022-07-15 14:36:59 -0700

    list of courses displayed correctly

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  template: `
+  <h2>{{title}}</h2>
+  <ul>
+    <li *ngFor='let course of courses'>
+      {{ course }}
+    </li>
+  </ul>
+  `
 })
 export class CoursesComponent implements OnInit {
-
-  constructor() { }
+  title = "List of Courses";
+  courses;
+  constructor() { 
+    let service = new CoursesService();
+    this.courses = service.getCourses();
+  }
 
   ngOnInit(): void {
   }
